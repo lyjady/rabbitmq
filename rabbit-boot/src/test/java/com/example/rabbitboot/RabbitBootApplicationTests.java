5e02c1b7f271f63a4f40d0a919c7354ae08c30e8
lyjady <lyj2780866735>
2019-10-17 22:02:13 +0800

    Initial commit

---
+++ rabbit-boot/src/test/java/com/example/rabbitboot/RabbitBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ rabbit-java/src/main/java/com/example/utils/CommonUtils.java
@@ -0,0 +1,31 @@
+package com.example.utils;
+
+
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author LinYongJin
+ * @date 2019/10/17 21:51
+ */
+public class CommonUtils {
+
+    public static Connection getRabbitMQConnection() throws IOException, TimeoutException {
+        //1.创建连接工厂
+        ConnectionFactory factory = new ConnectionFactory();
+        //2.设置主机
+        factory.setHost("localhost");
+        //3.设置端口号
+        factory.setPort(5672);
+        //4.设置用户名
+        factory.setUsername("guest");
+        //5.设置密码
+        factory.setPassword("guest");
+        //6.设置虚拟主机
+        factory.setVirtualHost("/test");
+        return factory.newConnection();
+    }
+}
