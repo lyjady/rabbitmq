fe95247528168a423499ed61e2f274912921cdf6
lyjady <lyj2780866735>
2019-10-26 16:29:12 +0800

    java publish subscription

---
+++ rabbit-boot-consumer/src/main/java/com/example/rabbitbootconsumer/RabbitBootConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbitbootconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitBootConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitBootConsumerApplication.class, args);
+	}
+
+}
---
+++ rabbit-boot-consumer/src/test/java/com/example/rabbitbootconsumer/RabbitBootConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitbootconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitBootConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
